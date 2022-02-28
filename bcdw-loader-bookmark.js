017620f60bb30aaf1eccd86a76a056384bcf80f1
noelle <noelle@GammaSYS.arcticline.home>
2022-02-27 22:54:13 -0600

    Add loaders.

---
+++ bcdw-loader-bookmark.js
@@ -0,0 +1,11 @@
+javascript: 
+(
+function ()
+{
+    let n = document.createElement("script");
+    n.setAttribute("language", "JavaScript");
+    n.setAttribute("crossorigin", "anonymous"); 
+    n.setAttribute("src", "https://mrbakucamp.github.io/BC-Diaper-Wetter/BCDiaperWetter.js?_=" + Date.now());
+    document.head.appendChild(n);
+}
+)();
---
+++ bcdw-loader-tampermonkey.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         BC Diaper Wetter
+// @namespace    https://www.bondageprojects.com/
+// @version      0.0.5
+// @description  A simple script that will automatically make a baby use their diaper
+// @author       Arctic Line
+// @match https://bondageprojects.elementfx.com/*
+// @match https://www.bondageprojects.elementfx.com/*
+// @match https://bondage-europe.com/*
+// @match https://www.bondage-europe.com/*
+// @match http://localhost:*/*
+// @icon data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant none
+// @run-at document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let n = document.createElement("script");
+    n.setAttribute("language", "JavaScript");
+    n.setAttribute("crossorigin", "anonymous");
+    n.setAttribute("src", "https://mrbakucamp.github.io/BC-Diaper-Wetter/BCDiaperWetter.js?_=" + Date.now());
+    document.head.appendChild(n);
+})();
